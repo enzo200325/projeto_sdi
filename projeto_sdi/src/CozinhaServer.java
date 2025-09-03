20e769c32ea677d953d821c5fd6ba3574d43cbc5
Matheus <matheus.a.cruzz@gmail.com>
2025-09-03 14:59:54 -0300

    Delete projeto_sdi directory

--- projeto_sdi/src/CozinhaServer.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
-
-public class CozinhaServer {
-    public static void main(String[] args) throws Exception {
-        CozinhaImpl cozinha = new CozinhaImpl();
-        System.out.println("Server Interfaces.Restaurante iniciado com sucesso");
-    }
-}
