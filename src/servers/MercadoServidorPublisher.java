693329d5e460def81732e33f971892321351e30c
math-amorim <matheus.a.cruzz@gmail.com>
2025-09-10 20:30:33 -0300

    Publisher

--- src/servers/MercadoServidorPublisher.java
+++ src/servers/MercadoServidorPublisher.java
@@ -1,4 +1,12 @@
 package servers;
 
+import implementacoes.MercadoServidorImpl;
+
+import javax.xml.ws.Endpoint;
+
 public class MercadoServidorPublisher {
+    public static void main(String[] args){
+        System.out.println("Publicando servidor do Mercado");
+        Endpoint.publish("http://127.0.0.1:9876/servers", new MercadoServidorImpl());
+    }
 }
